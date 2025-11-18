a6bfe6d64c570721d873c876f83a13dda6bf0ff4
dxminique <do.cofre@duocuc.cl>
2025-11-18 17:06:48 -0300

    se modifico el main

--- src/main/java/com/huertohogar/ms_ventas/MsVentasApplication.java
+++ src/main/java/com/huertohogar/ms_ventas/MsVentasApplication.java
@@ -2,13 +2,13 @@ package com.huertohogar.ms_ventas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.huertohogar.ms_ventas.repository")
 public class MsVentasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsVentasApplication.class, args);
 	}
-
 }
