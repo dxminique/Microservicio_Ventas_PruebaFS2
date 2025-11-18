40136850ba316a8df3e0f1557b7b8e59663565ae
dxminique <do.cofre@duocuc.cl>
2025-11-18 16:49:10 -0300

    Venta service

---
+++ src/main/java/com/huertohogar/ms_ventas/service/VentaService.java
@@ -0,0 +1,23 @@
+package com.huertohogar.ms_ventas.service;
+import com.huertohogar.ms_ventas.model.Venta;
+import com.huertohogar.ms_ventas.repository.VentaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VentaService {
+    private final VentaRepository ventaRepository;
+
+    public VentaService(VentaRepository ventaRepository) {
+        this.ventaRepository = ventaRepository;
+    }
+
+    public List<Venta> listarVentas() {
+        return ventaRepository.findAll();
+    }
+
+    public Venta crearVenta(Venta venta) {
+        return ventaRepository.save(venta);
+    }
+}
